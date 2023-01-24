0f64832fbea7b89d8093e3426b61874682402766
Eduardo Lopez <eduardolopezortiz95@gmail.com>
2023-01-24 13:42:53 -0600

    Priority for test scenarios

--- src/test/java/com/herokuapp/theinternet/NegativeTest.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTest.java
@@ -1,5 +1,7 @@
 package com.herokuapp.theinternet;
 
+import java.sql.Driver;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,13 +11,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NegativeTest {// class to test a negative test
-	@Test 
+	@Test(priority = 1)
 	public void incorrectusernameTest() {
-		
+
 		System.out.println("Starting incorrect user namame test");
 
 		// create driver
-		System.setProperty("webdriver.chrome.driver", "C:/Users/BI/eclipse-workspace/selenium-for-beginners/src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/BI/eclipse-workspace/selenium-for-beginners/src/main/resources/chromedriver.exe");
 		// new instance of driver
 		WebDriver driver = new ChromeDriver();
 
@@ -32,44 +35,96 @@ public class NegativeTest {// class to test a negative test
 
 		// maximize browser window
 		driver.manage().window().maximize();
-		
-		
-		//enter the wrong username
+
+		// enter the wrong username
 		WebElement username = driver.findElement(By.xpath("/html//input[@id='username']"));
 		username.sendKeys("tomsmith5");
 		sleep(1500);
-		
-		//enter password
+
+		// enter password
 		WebElement password = driver.findElement(By.name("password"));
 		password.sendKeys("SuperSecretPassword!");
 		sleep(4500);
-		
-		//click login button
+
+		// click login button
 		WebElement loginButton = driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
 		loginButton.click();
 		sleep(2500);
 
-		
-		
-	
-		
-		//success login message
+		// success login message
 		WebElement invalidUserNameMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
 		String expectedMessage = "Your username is invalid!";
 		String actualMessage = invalidUserNameMessage.getText();
-		Assert.assertTrue(actualMessage.contains(expectedMessage),"Actual message doesnt contain expected message.\nActual message: " + actualMessage 
-				+ "\nExpected message: " + expectedMessage);
-		
+		Assert.assertTrue(actualMessage.contains(expectedMessage),
+				"Actual message doesnt contain expected message.\nActual message: " + actualMessage
+						+ "\nExpected message: " + expectedMessage);
+
 		sleep(1500);
 		
-		
-	
-		//close browser
 		driver.quit();
+
 		
-		
-		
+
 	}
+	
+	@Test(priority = 2)
+	public void incorrectupasswordTest() {
+
+		System.out.println("<<<<Starting incorrect password test>>>>");
+
+		// create driver
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/BI/eclipse-workspace/selenium-for-beginners/src/main/resources/chromedriver.exe");
+		// new instance of driver
+		WebDriver driver = new ChromeDriver();
+
+		// sleep for 3 seconds
+		sleep(1000);
+
+		// open test page
+		String url = "https://the-internet.herokuapp.com/login";
+		driver.get(url);
+		System.out.println("<<<page is open>>>");
+
+		// sleep for 3 second
+		sleep(2000);
+
+		// maximize browser window
+		driver.manage().window().maximize();
+
+		// enter the wrong username
+		WebElement username = driver.findElement(By.xpath("/html//input[@id='username']"));
+		username.sendKeys("tomsmith");
+		sleep(1500);
+
+		// enter password
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("incorrectPassword!");
+		sleep(4500);
+
+		// click login button
+		WebElement loginButton = driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
+		loginButton.click();
+		sleep(2500);
+
+		// success login message
+		WebElement errorMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
+		String expectedMessage = "Your password is invalid!";
+		String actualMessage = errorMessage.getText();
+		Assert.assertTrue(actualMessage.contains(expectedMessage),
+				"Actual message doesnt contain expected message.\nActual message: " + actualMessage
+						+ "\nExpected message: " + expectedMessage);
+
+		sleep(1500);
+		
+		driver.quit();
+
+		
+
+	}
+	
+	
+	
 //method sleep
 	private void sleep(long m) {
 		try {
