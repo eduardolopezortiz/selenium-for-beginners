1320c79c4a3ee9030e27255f865cc07403cee78b
Eduardo Lopez <eduardolopezortiz95@gmail.com>
2023-01-25 00:59:45 -0600

    Parameters from Test Suite
    Creating several parameter for the two scenarios incorrectUsername and IncorrectPassword

--- src/test/java/com/herokuapp/theinternet/NegativeTest.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTest.java
@@ -8,14 +8,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.decorators.WebDriverDecorator;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NegativeTest {// class to test a negative test
+	
+	@Parameters({ "username", "password", "expectedMessage" })
 	@Test(priority = 1, groups = { "negativeTests", "smokeTests" })
 
-	public void incorrectusernameTest() {
+	public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
 
-		System.out.println("Starting incorrect user namame test");
+		System.out.println("Starting negativeLoginTest with "+ username +" and " + password);
 
 		// create driver
 		System.setProperty("webdriver.chrome.driver",
@@ -38,13 +41,13 @@ public class NegativeTest {// class to test a negative test
 		driver.manage().window().maximize();
 
 		// enter the wrong username
-		WebElement username = driver.findElement(By.xpath("/html//input[@id='username']"));
-		username.sendKeys("tomsmith5");
+		WebElement usernameElemnet = driver.findElement(By.xpath("/html//input[@id='username']"));
+		usernameElemnet.sendKeys(username);
 		sleep(1500);
 
 		// enter password
-		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("SuperSecretPassword!");
+		WebElement passwordElement = driver.findElement(By.name("password"));
+		passwordElement.sendKeys(password);
 		sleep(4500);
 
 		// click login button
@@ -54,11 +57,10 @@ public class NegativeTest {// class to test a negative test
 
 		// success login message
 		WebElement invalidUserNameMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
-		String expectedMessage = "Your username is invalid!";
 		String actualMessage = invalidUserNameMessage.getText();
-		Assert.assertTrue(actualMessage.contains(expectedMessage),
+		Assert.assertTrue(actualMessage.contains(expectedErrorMessage),
 				"Actual message doesnt contain expected message.\nActual message: " + actualMessage
-						+ "\nExpected message: " + expectedMessage);
+						+ "\nExpected message: " + expectedErrorMessage);
 
 		sleep(1500);
 		
@@ -68,61 +70,53 @@ public class NegativeTest {// class to test a negative test
 
 	}
 	
-	@Test(priority = 2, groups = { "negativeTests" })
-	public void incorrectupasswordTest() {
-
-		System.out.println("<<<<Starting incorrect password test>>>>");
-
-		// create driver
-		System.setProperty("webdriver.chrome.driver",
-				"C:/Users/BI/eclipse-workspace/selenium-for-beginners/src/main/resources/chromedriver.exe");
-		// new instance of driver
-		WebDriver driver = new ChromeDriver();
-
-		// sleep for 3 seconds
-		sleep(1000);
-
-		// open test page
-		String url = "https://the-internet.herokuapp.com/login";
-		driver.get(url);
-		System.out.println("<<<page is open>>>");
-
-		// sleep for 3 second
-		sleep(2000);
-
-		// maximize browser window
-		driver.manage().window().maximize();
-
-		// enter the wrong username
-		WebElement username = driver.findElement(By.xpath("/html//input[@id='username']"));
-		username.sendKeys("tomsmith");
-		sleep(1500);
-
-		// enter password
-		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("incorrectPassword!");
-		sleep(4500);
-
-		// click login button
-		WebElement loginButton = driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
-		loginButton.click();
-		sleep(2500);
-
-		// success login message
-		WebElement errorMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
-		String expectedMessage = "Your password is invalid!";
-		String actualMessage = errorMessage.getText();
-		Assert.assertTrue(actualMessage.contains(expectedMessage),
-				"Actual message doesnt contain expected message.\nActual message: " + actualMessage
-						+ "\nExpected message: " + expectedMessage);
-
-		sleep(1500);
-		
-		driver.quit();
-
-		
-
-	}
+	/*
+	 * @Test(priority = 2, groups = { "negativeTests" }) public void
+	 * incorrectupasswordTest() {
+	 * 
+	 * System.out.println("<<<<Starting incorrect password test>>>>");
+	 * 
+	 * // create driver System.setProperty("webdriver.chrome.driver",
+	 * "C:/Users/BI/eclipse-workspace/selenium-for-beginners/src/main/resources/chromedriver.exe"
+	 * ); // new instance of driver WebDriver driver = new ChromeDriver();
+	 * 
+	 * // sleep for 3 seconds sleep(1000);
+	 * 
+	 * // open test page String url = "https://the-internet.herokuapp.com/login";
+	 * driver.get(url); System.out.println("<<<page is open>>>");
+	 * 
+	 * // sleep for 3 second sleep(2000);
+	 * 
+	 * // maximize browser window driver.manage().window().maximize();
+	 * 
+	 * // enter the wrong username WebElement username =
+	 * driver.findElement(By.xpath("/html//input[@id='username']"));
+	 * username.sendKeys("tomsmith"); sleep(1500);
+	 * 
+	 * // enter password WebElement password =
+	 * driver.findElement(By.name("password"));
+	 * password.sendKeys("incorrectPassword!"); sleep(4500);
+	 * 
+	 * // click login button WebElement loginButton =
+	 * driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']"));
+	 * loginButton.click(); sleep(2500);
+	 * 
+	 * // success login message WebElement errorMessage =
+	 * driver.findElement(By.xpath("/html//div[@id='flash']")); String
+	 * expectedMessage = "Your password is invalid!"; String actualMessage =
+	 * errorMessage.getText();
+	 * Assert.assertTrue(actualMessage.contains(expectedMessage),
+	 * "Actual message doesnt contain expected message.\nActual message: " +
+	 * actualMessage + "\nExpected message: " + expectedMessage);
+	 * 
+	 * sleep(1500);
+	 * 
+	 * driver.quit();
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 	
 	
 	
