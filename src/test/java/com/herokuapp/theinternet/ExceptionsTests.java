af4366934dcec8746b493cae2bfe8dc92d32acaa
Eduardo Lopez <eduardolopezortiz95@gmail.com>
2023-02-03 17:04:41 -0600

    ElementNotInteractableException

--- src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
+++ src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
@@ -24,7 +24,7 @@ public class ExceptionsTests {
 
 	@Parameters({ "browser" })
 	@BeforeMethod(alwaysRun = true)
-	// si no hay un parametro que venga del LoginTests.xml en su lugar asignara 
+	// si no hay un parametro que venga del LoginTests.xml en su lugar asignara
 	// chrome a la variable browser
 	private void setUp(@Optional("chrome") String browser) {
 		// TODO Auto-generated method stub
@@ -56,43 +56,66 @@ public class ExceptionsTests {
 		}
 
 		// sleep for 3 second
-		
 
 		// maximize browser window
 		driver.manage().window().maximize();
-		
+
 	}
-	
-	
+
 	@Test
 	public void noSuchElementExceptionTest() {
-		
-		
-		//Test case 1: noSuchElementException
-		
-		//Open page
+
+		// Test case 1: noSuchElementException
+
+		// Open page
 		driver.get("https://practicetestautomation.com/practice-test-exceptions/");
-		
-		
-		//Click Add button
+
+		// Click Add button
 		WebElement addButtonElement = driver.findElement(By.xpath("/html//button[@id='add_btn']"));
 		addButtonElement.click();
-		
-		
-		//Explicit wait 
+
+		// Explicit wait
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement row2Input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='rows']/div[3]/div[@class='row']/input[@type='text']")));
-		
-		
-		
-		//Verify Row 2 input field is displayed
-		Assert.assertTrue(row2Input.isDisplayed(),"Row 2 is not diplayed");
-		
-			
-		
-		
-		
-		
+		WebElement row2Input = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//div[@id='rows']/div[3]/div[@class='row']/input[@type='text']")));
+
+		// Verify Row 2 input field is displayed
+		Assert.assertTrue(row2Input.isDisplayed(), "Row 2 is not diplayed");
+
+	}
+
+	@Test
+	public void elementNotInteractableException() {
+
+		// Test case 2: ElementNotInteractableException
+
+		// Open page
+		driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+
+		// Click Add button
+		WebElement addButtonElement = driver.findElement(By.id("add_btn"));
+		addButtonElement.click();
+
+		// Wait for the second row to load
+		// Explicit wait
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement row2Input = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='row2']/input")));
+
+		// Type text into the second input field
+		row2Input.sendKeys("Sushi");
+
+		// Push Save button using locator By.name(“Save”)
+		WebElement saveButton = driver.findElement(By.xpath("//div[@id='row2']/button[@name='Save']"));
+		saveButton.click();
+
+		// Verify text saved
+		// 1. we need to create another web element to locate the message
+		WebElement confirmationMessage = driver.findElement(By.id("confirmation"));
+		// 2. Save message into a variable name messageText
+		String messageText = confirmationMessage.getText();
+		Assert.assertEquals(messageText, "Row 2 was saved", "Confirmation message text is not expected");
+
 	}
 
 	@AfterMethod(alwaysRun = true)
